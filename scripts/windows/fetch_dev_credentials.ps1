4bde25e598cf11ba7ef593fa64422efc5ff1032c
Tianhao Zhou <tianhaoz@umich.edu>
2020-09-15 21:23:59 -0700

    dev: add scripts for windows (#3)

---
+++ scripts/windows/fetch_dev_credentials.ps1
@@ -0,0 +1,8 @@
+if (Test-Path env:PROJ_ROOT) {
+    & "$env:WINDOWS_HELPER_SCRIPT_DIR\fetch_credential_repository.ps1"
+    & "$env:WINDOWS_HELPER_SCRIPT_DIR\maybe_clean_local_env.ps1"
+    Copy-Item -Path "$env:CRED_LOCATION\dev\.env" -Destination "$env:PROJ_ROOT\.env"
+    Write-Output "Development credentials copied!"
+} else {
+    Write-Output "PROJ_ROOT not found in environment, please run set_environment.ps1 first."
+}
