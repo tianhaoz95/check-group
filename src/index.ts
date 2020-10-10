d2eb91697771bd4610d59b2615bbee61cdeaf928
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-10 14:12:34 -0700

    fix: probot 10 breaking changes

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
-import { checkRunEventHandler, pullRequestEventHandler } from "./handlers";
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Application } from "probot";
+import { Application, Context } from "probot";
 /* eslint-enable @typescript-eslint/no-unused-vars */
+import { checkRunEventHandler, pullRequestEventHandler } from "./handlers";
 import prettyjson from "prettyjson";
 
 export = (app: Application): void => {
-  app.on("pull_request", async (context) => {
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  app.on("pull_request", async (context: Context<any>) => {
     try {
       await pullRequestEventHandler(context);
     } catch (err) {
       context.log.error(prettyjson.render(err));
     }
   });
+  /* eslint-enable @typescript-eslint/no-explicit-any */
 
-  app.on("check_run", async (context) => {
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  app.on("check_run", async (context: Context<any>) => {
     try {
       await checkRunEventHandler(context);
     } catch (err) {
       context.log.error(prettyjson.render(err));
     }
   });
+  /* eslint-enable @typescript-eslint/no-explicit-any */
 };
