c497ed816e546d6dd57b85c531fe03d8113d9529
Tianhao Zhou <tianhaoz@umich.edu>
2021-05-07 01:08:28 -0700

    chore: add vercel deployment

---
+++ api/github/webhooks/index.js
@@ -0,0 +1,11 @@
+import { createNodeMiddleware, createProbot } from "probot";
+
+import app from "../../../lib/index";
+
+const probot = createProbot({
+  defaults: {
+    webhookPath: "/api/github/webhooks",
+  },
+});
+
+export default createNodeMiddleware(app, { probot });
