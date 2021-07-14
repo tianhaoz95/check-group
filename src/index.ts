cec0a5d18eb26b3c19ba36bb3e95ec46f593c48f
Tianhao Zhou <tianhaoz@umich.edu>
2021-07-14 16:30:59 +0000

    feat: add sentry infra (#469)

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,38 @@
+import * as Sentry from "@sentry/node";
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Context, Probot } from "probot";
 /* eslint-enable @typescript-eslint/no-unused-vars */
 import { checkRunEventHandler, pullRequestEventHandler } from "./handlers";
+import dotenv from "dotenv";
 import prettyjson from "prettyjson";
 
+// TODO(@tianhaoz95): consolidate this into a location called
+// third_party to indicate that we are not in control of whether
+// this works or not.
+// For setup details, see:
+// https://docs.sentry.io/platforms/node
+dotenv.config();
+Sentry.init({
+  dsn: process.env["SENTRY_DSN"],
+  tracesSampleRate: 1.0,
+});
+
 export = (app: Probot): void => {
-  /* eslint-disable @typescript-eslint/no-explicit-any */
   app.on("pull_request", async (context: Context<"pull_request">) => {
     try {
       await pullRequestEventHandler(context);
     } catch (err) {
       context.log.error(prettyjson.render(err));
+      Sentry.captureException(err);
     }
   });
-  /* eslint-enable @typescript-eslint/no-explicit-any */
 
-  /* eslint-disable @typescript-eslint/no-explicit-any */
   app.on("check_run", async (context: Context<"check_run">) => {
     try {
       await checkRunEventHandler(context);
     } catch (err) {
       context.log.error(prettyjson.render(err));
+      Sentry.captureException(err);
     }
   });
-  /* eslint-enable @typescript-eslint/no-explicit-any */
 };
