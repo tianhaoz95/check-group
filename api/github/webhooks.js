31994b91ff2ffd953b28a4296f7ca943f4ab2516
Tianhao Zhou <tianhaoz@umich.edu>
2021-09-03 23:42:54 +0000

    fix: specify file

--- api/github/webhooks.js
+++ api/github/webhooks.js
@@ -1,5 +1,5 @@
 import { createNodeMiddleware, createProbot } from "probot";
-import app from "../../lib";
+import app from "../../lib/index";
 
 module.exports = createNodeMiddleware(app, {
   probot: createProbot(),
