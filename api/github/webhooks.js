42425669f6f4b69d48fb996e995e411994ef60f8
Tianhao Zhou <tianhaoz@umich.edu>
2021-09-03 23:33:21 +0000

    fix: change location

--- api/github/webhooks.js
+++ api/github/webhooks.js
@@ -1,7 +1,5 @@
-// For more details on the suggested way to deploy to Vercel:
-//  https://probot.github.io/docs/deployment/#vercel
 import { createNodeMiddleware, createProbot } from "probot";
-import app from "../../../lib";
+import app from "../../lib";
 
 module.exports = createNodeMiddleware(app, {
   probot: createProbot(),
