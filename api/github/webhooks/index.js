0b117051ad35c1496a9c6cd6175bf042a7fb8b52
Tianhao Zhou <tianhaoz@umich.edu>
2021-09-04 06:42:45 +0000

    fix: use ts

--- api/github/webhooks/index.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { createNodeMiddleware, createProbot } = require("probot");
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const app = require("../../../lib/index");
-const probot = createProbot();
-
-module.exports = createNodeMiddleware(app, { probot, webhooksPath: "/api/github/webhooks" });
