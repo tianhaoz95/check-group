6d3b85f713e606ef931848f6e1bd69a552a57ef0
Tianhao Zhou <tianhaoz@umich.edu>
2021-10-22 14:19:00 -0700

    feat: add warning for missing checks for better debugging experience (#610)
    When the subproject in the configuration file contains no paths or no checks, the current behavior is that the check will fail silently as indicated in #593. This behavior is confusing for developers since the impression will be that nothing went wrong. We want to call out this issue. However, it is valid for someone to leave the checks empty for now and add more things later as the project evolves, so the behavior chosen is to add warnings at the end of the detail status to let the users know what happened.

--- src/utils/user_config_parser/populate_subprojects.test.ts
+++ src/utils/user_config_parser/populate_subprojects.test.ts
@@ -1,8 +1,131 @@
-import { CheckGroupConfig } from "../../types";
+import { CheckGroupConfig, SubProjConfig } from "../../types";
+import {
+  parseProjectChecks,
+  parseProjectId,
+  parseProjectPaths,
+  populateSubprojects,
+} from "./populate_subprojects";
 import { getDefaultConfig } from "./default_config";
-import { populateSubprojects } from "./populate_subprojects";
 
 describe("Populate subprojects helper tests", () => {
+  test("Missing ID should fail", () => {
+    const rawConfig: Record<string, unknown> = {
+      "checks": [],
+      "paths": [],
+    };
+    const config: SubProjConfig = {
+      checks: [],
+      id: "Unknown",
+      paths: [],
+    };
+    expect(() => parseProjectId(rawConfig, config)).toThrow();
+  });
+
+  test("Correctly parse project ID", () => {
+    const rawConfig: Record<string, unknown> = {
+      "checks": [],
+      "id": "foo",
+      "paths": [],
+    };
+    const config: SubProjConfig = {
+      checks: [],
+      id: "Unknown",
+      paths: [],
+    };
+    parseProjectId(rawConfig, config);
+    expect(config.id).toMatch("foo");
+  });
+
+  test("Empty paths should give warnings", () => {
+    const rawConfig: Record<string, unknown> = {
+      "checks": [],
+      "id": "foo",
+      "paths": [],
+    };
+    const config: SubProjConfig = {
+      checks: [],
+      id: "Unknown",
+      paths: [],
+    };
+    const overallConfig: CheckGroupConfig = {
+      customServiceName: "",
+      debugInfo: [],
+      subProjects: [],
+    };
+    const expectedWarningCnt = 1;
+    parseProjectPaths(rawConfig, config, overallConfig);
+    expect(config.paths).toEqual([]);
+    expect(overallConfig.debugInfo.length).toEqual(expectedWarningCnt);
+  });
+
+  test("Parses correct paths", () => {
+    const rawConfig: Record<string, unknown> = {
+      "checks": [],
+      "id": "foo",
+      "paths": ["**", "docs/**"],
+    };
+    const config: SubProjConfig = {
+      checks: [],
+      id: "Unknown",
+      paths: [],
+    };
+    const overallConfig: CheckGroupConfig = {
+      customServiceName: "",
+      debugInfo: [],
+      subProjects: [],
+    };
+    const expectedWarningCnt = 0;
+    const expectedPathCnt = 2;
+    parseProjectPaths(rawConfig, config, overallConfig);
+    expect(config.paths.length).toEqual(expectedPathCnt);
+    expect(overallConfig.debugInfo.length).toEqual(expectedWarningCnt);
+  });
+
+  test("Empty checks should give warnings", () => {
+    const rawConfig: Record<string, unknown> = {
+      "checks": [],
+      "id": "foo",
+      "paths": [],
+    };
+    const config: SubProjConfig = {
+      checks: [],
+      id: "Unknown",
+      paths: [],
+    };
+    const overallConfig: CheckGroupConfig = {
+      customServiceName: "",
+      debugInfo: [],
+      subProjects: [],
+    };
+    const expectedWarningCnt = 1;
+    parseProjectChecks(rawConfig, config, overallConfig);
+    expect(config.paths).toEqual([]);
+    expect(overallConfig.debugInfo.length).toEqual(expectedWarningCnt);
+  });
+
+  test("Parses correct checks", () => {
+    const rawConfig: Record<string, unknown> = {
+      "checks": ["foo", "bar"],
+      "id": "foo",
+      "paths": [],
+    };
+    const config: SubProjConfig = {
+      checks: [],
+      id: "Unknown",
+      paths: [],
+    };
+    const overallConfig: CheckGroupConfig = {
+      customServiceName: "",
+      debugInfo: [],
+      subProjects: [],
+    };
+    const expectedWarningCnt = 0;
+    const expectedCheckCnt = 2;
+    parseProjectChecks(rawConfig, config, overallConfig);
+    expect(config.checks.length).toEqual(expectedCheckCnt);
+    expect(overallConfig.debugInfo.length).toEqual(expectedWarningCnt);
+  });
+
   test("Sanity check with empty config", () => {
     const configData: Record<string, unknown> = {
       "subprojects": [],
@@ -17,15 +140,9 @@ describe("Populate subprojects helper tests", () => {
     const configData: Record<string, unknown> = {
       "subprojects": [
         {
-          "checks": [
-            "test-check-1",
-            "test-check-2",
-          ],
+          "checks": ["test-check-1", "test-check-2"],
           "id": "test-proj",
-          "paths": [
-            "/example/path/1",
-            "/example/path/2",
-          ],
+          "paths": ["/example/path/1", "/example/path/2"],
         },
       ],
     };
@@ -37,8 +154,29 @@ describe("Populate subprojects helper tests", () => {
     const expectedProjectPathsCnt = 2;
     expect(config.subProjects.length).toEqual(expectedSubprojectCnt);
     // eslint-disable-next-line security/detect-object-injection
-    expect(config.subProjects[availableIndex].checks.length).toEqual(expectedProjectChecksCnt);
+    expect(config.subProjects[availableIndex].checks.length).toEqual(
+      expectedProjectChecksCnt,
+    );
     // eslint-disable-next-line security/detect-object-injection
-    expect(config.subProjects[availableIndex].paths.length).toEqual(expectedProjectPathsCnt);
+    expect(config.subProjects[availableIndex].paths.length).toEqual(
+      expectedProjectPathsCnt,
+    );
+  });
+
+  test("Misconfigured project missing checks should emit warnings", () => {
+    const configData: Record<string, unknown> = {
+      "subprojects": [
+        {
+          "id": "test-proj",
+          "paths": ["/example/path/1", "/example/path/2"],
+        },
+      ],
+    };
+    const config: CheckGroupConfig = getDefaultConfig();
+    populateSubprojects(configData, config);
+    const expectedSubprojectCnt = 1;
+    const expectedWarningCnt = 1;
+    expect(config.subProjects.length).toEqual(expectedSubprojectCnt);
+    expect(config.debugInfo.length).toEqual(expectedWarningCnt);
   });
 });
--- src/utils/user_config_parser/populate_subprojects.ts
+++ src/utils/user_config_parser/populate_subprojects.ts
@@ -1,44 +1,123 @@
-import { CheckGroupConfig, SubProjCheck, SubProjConfig, SubProjPath } from "../../types";
+import {
+  CheckGroupConfig,
+  SubProjCheck,
+  SubProjConfig,
+  SubProjPath,
+} from "../../types";
+
+/**
+ * Parses the structured ID into sub-project data from the raw user config.
+ *
+ * The ID is required for the sub-project since it is hard to give the user
+ * any useful information for debugging if the ID that is used to identify the
+ * location of the issue is missing. In this case, it will be better to bail.
+ *
+ * @param subprojData The raw data from the config file.
+ * @param subprojConfig The structured data for the sub-project.
+ */
+export function parseProjectId(
+  subprojData: Record<string, unknown>,
+  subprojConfig: SubProjConfig,
+): void {
+  if ("id" in subprojData) {
+    subprojConfig.id = subprojData["id"] as string;
+  } else {
+    throw Error("Essential field missing from config: sub-project ID");
+  }
+}
+
+export function parseProjectPaths(
+  subprojData: Record<string, unknown>,
+  subprojConfig: SubProjConfig,
+  config: CheckGroupConfig,
+): void {
+  if ("paths" in subprojData && subprojData["paths"] !== null) {
+    const projPaths: SubProjPath[] = [];
+    const locations: string[] = subprojData["paths"] as string[];
+    locations.forEach((loc) => {
+      projPaths.push({
+        location: loc,
+      });
+    });
+    const minPathCnt = 0;
+    if (projPaths.length > minPathCnt) {
+      subprojConfig.paths = projPaths;
+    } else {
+      config.debugInfo.push({
+        configError: true,
+        configErrorMsg: "Paths is empty.",
+      });
+    }
+  } else {
+    config.debugInfo.push({
+      configError: true,
+      configErrorMsg: `:warning: Essential fields missing from config for project ${subprojConfig.id}: paths`,
+    });
+  }
+}
+
+export function parseProjectChecks(
+  subprojData: Record<string, unknown>,
+  subprojConfig: SubProjConfig,
+  config: CheckGroupConfig,
+): void {
+  if ("checks" in subprojData && subprojData["checks"] !== null) {
+    const projChecks: SubProjCheck[] = [];
+    const checksData: string[] = subprojData["checks"] as string[];
+    checksData.forEach((checkId) => {
+      projChecks.push({
+        id: checkId,
+      });
+    });
+    const minPathCnt = 0;
+    if (projChecks.length > minPathCnt) {
+      subprojConfig.checks = projChecks;
+    } else {
+      config.debugInfo.push({
+        configError: true,
+        configErrorMsg: "Checks is empty.",
+      });
+    }
+  } else {
+    config.debugInfo.push({
+      configError: true,
+      configErrorMsg: `:warning: Essential fields missing from config for project ${subprojConfig.id}: checks`,
+    });
+  }
+}
 
 /**
  * Parse user config file and populate subprojects
  * @param {Record<string, unknown>} configData
  * @param {CheckGroupConfig} config
  **/
-export function populateSubprojects(configData: Record<string, unknown>, config: CheckGroupConfig): void {
+export function populateSubprojects(
+  configData: Record<string, unknown>,
+  config: CheckGroupConfig,
+): void {
   if ("subprojects" in configData) {
     const subProjectsData = configData["subprojects"] as Record<
-            string,
-            unknown
-        >[];
+      string,
+      unknown
+    >[];
     subProjectsData.forEach((subprojData) => {
-      if (
-        "id" in subprojData &&
-                "paths" in subprojData &&
-                "checks" in subprojData
-      ) {
-        const projPaths: SubProjPath[] = [];
-        const locations: string[] = subprojData["paths"] as string[];
-        locations.forEach((loc) => {
-          projPaths.push({
-            location: loc,
-          });
-        });
-        const projChecks: SubProjCheck[] = [];
-        const checksData: string[] = subprojData["checks"] as string[];
-        checksData.forEach((checkId) => {
-          projChecks.push({
-            id: checkId,
-          });
-        });
-        const subprojConfig: SubProjConfig = {
-          checks: projChecks,
-          id: subprojData["id"] as string,
-          paths: projPaths,
-        };
+      const subprojConfig: SubProjConfig = {
+        checks: [],
+        id: "Unknown",
+        paths: [],
+      };
+      try {
+        parseProjectId(subprojData, subprojConfig);
+        parseProjectPaths(subprojData, subprojConfig, config);
+        parseProjectChecks(subprojData, subprojConfig, config);
         config.subProjects.push(subprojConfig);
-      } else {
-        throw Error("id, paths, checks not all found in subproject");
+      } catch (err) {
+        config.debugInfo.push({
+          configError: true,
+          configErrorMsg: `Error adding sub-project from data:\n ${JSON.stringify(
+            subprojData,
+          )}`,
+        });
       }
     });
   } else {
